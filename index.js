617efa3c2f1c86890096c47fc09a97ff549ac282
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-02 03:17:53 +0300

    commit tree 02dda236faeeb95a5872c09fa5daad38d4f91057

---
+++ index.js
@@ -0,0 +1,56 @@
+function add(){
+    // There is no time to do this
+}
+
+function add(x,y){
+    return x+y;
+}
+console.log(add(4,9));
+
+function subtract(){
+    // There is no time for this one too!;
+}
+function multiply(){
+    // I dont like multiplication;
+}
+function divide(){
+    // Division is borring;
+}
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply(56,890092));
+
+function subtract(a,b){
+    return a-b;
+}
+console.log(subtract(5443453,652));
+
+function divide(a,b){
+    return a/b;
+}
+console.log(90,10);
+
+
+
+
+function increment(number = 16){
+    return (number +=1);
+}
+console.log(increment());
+
+
+function decrement(number = 16){
+    return (number -=1);
+}
+console.log(decrement());
+
+function makeInt (n){
+    return parseInt(n,10);
+}
+console.log(makeInt(9000));
+
+function preserveDecimal (n){
+    return parseFloat(n,10);
+}
+console.log(preserveDecimal(890.089372));
